66e1d388c8dce625349bf091380d6c4a9c7ae562
xmyoot <xmyoot@yahoo.com>
2017-04-17 09:08:16 -0700

    added adventurer class with virtual function attack

--- Project 3 Starting Point/Project 3 Starting Point/Adventurer.h
+++ Project 3 Starting Point/Project 3 Starting Point/Adventurer.h
@@ -1,8 +1,13 @@
 #pragma once
+#include <string>
 class Adventurer
 {
+private:
+	std::string _name;
 public:
-	Adventurer();
+	Adventurer(std::string name) { _name = name; };
+	std::string GetName() const { return _name; }
+	virtual std::string Attack() {};
 	~Adventurer();
 };
 
